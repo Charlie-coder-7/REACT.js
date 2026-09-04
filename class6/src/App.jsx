278054f43519ec13698d8e671066c5eb7fdba8e5
ACHYUTA SAHOO <sahooachyuta168@gmail.com>
2026-09-05 01:11:38 +0530

    Two way binding in react

--- class6/src/App.jsx
+++ class6/src/App.jsx
@@ -1,21 +1,83 @@
-import React from 'react'
+// import React from 'react'
+
+// const App = () => {
+//   const submitHandler=(e)=>{
+//     e.preventDefault();
+//     console.log("form submitted");
+
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//         submitHandler(e);
+//       }}>
+//         <input type="text" placeholder='Enter your name'/>
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  const submitHandler=(e)=>{
-    e.preventDefault();
-    console.log("form submitted");
+  const [name, setname] = useState("");
+  const [email, setemail] = useState('')
+  const [allusers, setallusers] = useState([])
+   const formHandler = (e) => {
+     e.preventDefault();
+    //  const newAllusers = [...allusers]
+    //  newAllusers.push(name)
+    //  console.log(newAllusers); 
+    //  setallusers(newAllusers)
+    const oldUsers = [...allusers]
+    oldUsers.push({name,email})
+    setallusers(oldUsers)
+    console.log(oldUsers);
     
-  }
+     setname("");
+     setemail("");
+   };
   return (
     <div>
-      <form onSubmit={(e)=>{
-        submitHandler(e);
-      }}>
-        <input type="text" placeholder='Enter your name'/>
+      <form
+        onSubmit={(e) => {
+          formHandler(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          value={name}
+          required
+          onChange={(e) => {
+            setname(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Enter Your Email"
+          value={email}
+          required
+          onChange={(e) => {
+            setemail(e.target.value);
+          }}
+        />
+
         <button>Submit</button>
       </form>
+      {allusers.map(function (elem,idx) {
+        return (
+          <div key={idx}>
+            <h1>{elem.name}</h1>
+            <h1>{elem.email}</h1>
+          </div>
+        )
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
