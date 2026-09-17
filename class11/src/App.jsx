0fd483d0c739945df367d0ca798b45de4af3be4d
ACHYUTA SAHOO <sahooachyuta168@gmail.com>
2026-09-17 22:21:44 +0530

    Understanding Routing(1.Setting up React Router DOM)

---
+++ class11/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl underline font-bold absolute top-1/2 left-1/2 -translate-1/2'>App</div>
+  )
+}
+
+export default App
